95f2374b3fa7e5ecd6e1d0e28deda804a087ad8c
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-04-30 23:19:30 -0400

    Change define name for common options file

--- src/common/options.h
+++ src/common/options.h
@@ -1,5 +1,5 @@
-#ifndef SYNCAIDE_OPTIONS_H
-#define SYNCAIDE_OPTIONS_H
+#ifndef SYNCAIDE_COMMON_OPTIONS_H
+#define SYNCAIDE_COMMON_OPTIONS_H
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/algorithm/string.hpp>
@@ -40,4 +40,4 @@ namespace common {
     };
 }
 
-#endif //SYNCAIDE_OPTIONS_H
+#endif //SYNCAIDE_COMMON_OPTIONS_H
