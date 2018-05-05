da969e375cb414a5eacb3a4485b79ef2a3e3aca1
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-05-05 13:39:24 -0400

    Refactor detector.hpp file to better structure the classes

--- src/server/detector.h
+++ src/server/detector.h
@@ -14,232 +14,139 @@
 #include <boost/asio/strand.hpp>
 #include <boost/logic/tribool.hpp>
 
+#define RESULT_TYPE(ct, sig) \
+  typename ::boost::asio::async_result< \
+    typename ::boost::asio::decay<ct>::type, sig>::return_type
+
+#define HANDLER_TYPE(ct, sig) \
+  typename ::boost::asio::async_result< \
+    typename ::boost::asio::decay<ct>::type, sig>::completion_handler_type
+
+#define HANDLER_SIG void(boost::beast::error_code, boost::tribool)
+
+#define STREAM_EXEC_TYPE decltype(declval<Stream &>().get_executor())
+
 using namespace std;
-
-template<class ConstBufferSequence>
-boost::tribool is_ssl_handshake(ConstBufferSequence const &buffers) {
-    static_assert(
-        boost::asio::is_const_buffer_sequence<ConstBufferSequence>::value,
-        "ConstBufferSequence requirements not met"
-    );
-
-    if (boost::asio::buffer_size(buffers) < 1) {
-        return boost::indeterminate;
-    }
-
-    unsigned char v;
-    boost::asio::buffer_copy(boost::asio::buffer(&v, 1), buffers);
-
-    if (v != 0x16) {
-        return false;
-    }
-
-    if (boost::asio::buffer_size(buffers) < 4) {
-        return boost::indeterminate;
-    }
-
-    return true;
-}
-
-template<class SyncReadStream, class DynamicBuffer>
-boost::tribool detect_ssl(
-    SyncReadStream &stream,
-    DynamicBuffer &buffer,
-    boost::beast::error_code &code
-) {
-    namespace beast = boost::beast;
-
-    static_assert(
-        beast::is_sync_read_stream<SyncReadStream>::value,
-        "SyncReadStream requirements not met"
-    );
-
-    static_assert(
-        boost::asio::is_dynamic_buffer<DynamicBuffer>::value,
-        "DynamicBuffer requirements not met"
-    );
-
-    while (true) {
-        auto const result = is_ssl_handshake(buffer.data());
-
-        if (!boost::indeterminate(result)) {
-            code.assign(0, code.category());
-            return result;
-        }
-
-        BOOST_ASSERT(buffer.size() < 4);
-        auto const mutable_buffer = buffer.prepare(
-            beast::read_size(buffer, 1536)
-        );
-
-        size_t const bytes_transferred = stream.read_some(mutable_buffer, code);
-
-        if (code) {
-            break;
-        }
-
-        buffer.commit(bytes_transferred);
-    }
-
-    return false;
-}
-
-template<class AsyncReadStream, class DynamicBuffer, class Handler>
-class detect_ssl_op;
-
-template<class AsyncReadStream, class DynamicBuffer, class CompletionToken>
-BOOST_ASIO_INITFN_RESULT_TYPE(
-    CompletionToken,
-    void(boost::beast::error_code, boost::tribool)
-)
-async_detect_ssl(
-    AsyncReadStream &stream,
-    DynamicBuffer &buffer,
-    CompletionToken &&token
-) {
-    namespace beast = boost::beast;
-
-    static_assert(
-        beast::is_async_read_stream<AsyncReadStream>::value,
-        "SyncReadStream requirements not met"
-    );
-
-    static_assert(
-        boost::asio::is_dynamic_buffer<DynamicBuffer>::value,
-        "DynamicBuffer requirements not met"
-    );
-
-    boost::asio::async_completion<
-        CompletionToken,
-        void(beast::error_code, boost::tribool)
-    > init{token};
-
-    detect_ssl_op<
-        AsyncReadStream,
-        DynamicBuffer,
-        BOOST_ASIO_HANDLER_TYPE(
-            CompletionToken,
-            void(beast::error_code, boost::tribool)
-        )
-    > {stream, buffer, init.completion_handler}(beast::error_code{}, 0);
-
-    return init.result.get();
-}
-
-template<class AsyncReadStream, class DynamicBuffer, class Handler>
-class detect_ssl_op {
-    int step_ = 0;
-
-    AsyncReadStream &stream_;
-    DynamicBuffer &buffer_;
-    Handler handler_;
-    boost::tribool result_ = false;
-
-public:
-    detect_ssl_op(detect_ssl_op const &) = default;
-
-    template<class DeducedHandler>
-    detect_ssl_op(
-        AsyncReadStream &stream,
-        DynamicBuffer &buffer,
-        DeducedHandler &&handler
-    ) : stream_(stream),
-        buffer_(buffer),
-        handler_(forward<DeducedHandler>(handler)) {}
-
-    using allocator_type = boost::asio::associated_allocator_t<Handler>;
-
-    allocator_type get_allocator() const noexcept {
-        return boost::asio::get_associated_allocator(handler_);
-    }
-
-    using executor_type = boost::asio::associated_executor_t<
-        Handler,
-        decltype(declval<AsyncReadStream &>().get_executor())
-    >;
-
-    executor_type get_executor() const noexcept {
-        return boost::asio::get_associated_executor(
-            handler_,
-            stream_.get_executor()
-        );
-    }
-
-    friend bool asio_handler_is_continuation(detect_ssl_op *op) {
-        using boost::asio::asio_handler_is_continuation;
-
-        return op->step_ > 2
-               || asio_handler_is_continuation(addressof(op->handler_));
-    }
-
-    void operator()(boost::beast::error_code code, size_t bytes_transferred);
-};
-
-template<class AsyncStream, class DynamicBuffer, class Handler>
-void detect_ssl_op<AsyncStream, DynamicBuffer, Handler>::operator()(
-    boost::beast::error_code code,
-    size_t bytes_transferred
-) {
-    namespace beast = boost::beast;
-
-    switch (step_) {
-        case 0:
-            result_ = is_ssl_handshake(buffer_.data());
-
-            if (!boost::indeterminate(result_)) {
-                step_ = 1;
-                return boost::asio::post(
-                    stream_.get_executor(),
-                    beast::bind_handler(move(*this), code, 0));
-            }
-
-            BOOST_ASSERT(buffer_.size() < 4);
-
-            step_ = 2;
-
-        do_read:
-            return stream_.async_read_some(
-                buffer_.prepare(beast::read_size(buffer_, 1536)),
-                move(*this)
-            );
-
-        case 1:
-            break;
-
-        case 2:
-            step_ = 3;
-                BOOST_FALLTHROUGH;
-
-        case 3:
-            if (code) {
-                result_ = false;
-                break;
-            }
-
-            buffer_.commit(bytes_transferred);
-            result_ = is_ssl_handshake(buffer_.data());
-
-            if (!boost::indeterminate(result_)) {
-                break;
-            }
-
-            goto do_read;
-    }
-
-    handler_(code, result_);
-}
 
 namespace server {
     namespace asio = boost::asio;
     namespace beast = boost::beast;
     using boost::system::error_code;
     using boost::beast::flat_buffer;
+    using boost::beast::read_size;
+    using boost::asio::async_completion;
+    using boost::asio::is_const_buffer_sequence;
+    using boost::asio::get_associated_allocator;
+    using boost::asio::associated_allocator_t;
+    using boost::asio::associated_executor_t;
+    using boost::asio::get_associated_executor;
+    using boost::asio::buffer_size;
+    using boost::asio::buffer_copy;
+    using boost::beast::bind_handler;
+    using boost::asio::buffer;
     using boost::asio::ip::tcp;
     using boost::asio::io_context;
     using boost::asio::ssl::context;
     using boost::asio::bind_executor;
     using boost::asio::strand;
+    using boost::asio::post;
+    using boost::indeterminate;
     using boost::tribool;
+
+    template<class Stream, class Buffer, class Handler>
+    class DetectSslOp {
+    private:
+        Stream &_stream;
+        Buffer &_buffer;
+        Handler _handler;
+        tribool _result = false;
+        int _step = 0;
+
+    public:
+        DetectSslOp(DetectSslOp const &) = default;
+
+        template<class Deduce>
+        DetectSslOp(Stream &stream, Buffer &buffer, Deduce &&handler) :
+            _stream(stream),
+            _buffer(buffer),
+            _handler(forward<Deduce>(handler)) {}
+
+        using exec_type = associated_executor_t<Handler, STREAM_EXEC_TYPE>;
+        using alloc_type = associated_allocator_t<Handler>;
+
+        alloc_type get_allocator() const noexcept {
+            return get_associated_allocator(_handler);
+        }
+
+        exec_type get_executor() const noexcept {
+            return get_associated_executor(_handler, _stream.get_executor());
+        }
+
+        friend bool asio_handler_is_continuation(DetectSslOp *op) {
+            using boost::asio::asio_handler_is_continuation;
+            return op->_step > 2
+                   || asio_handler_is_continuation(addressof(op->_handler));
+        }
+
+        template<class ConstBufferSequence>
+        tribool is_ssl_handshake(ConstBufferSequence const &buffers) {
+            if (buffer_size(buffers) < 1) return indeterminate;
+
+            unsigned char v;
+            buffer_copy(buffer(&v, 1), buffers);
+
+            if (v != 0x16) return false;
+            if (buffer_size(buffers) < 4) return indeterminate;
+
+            return true;
+        }
+
+        void operator()(error_code code, size_t bytes_transferred) {
+            switch (_step) {
+                case 0:
+                    _result = is_ssl_handshake(_buffer.data());
+                    if (!indeterminate(_result)) {
+                        _step = 1;
+                        return post(
+                            _stream.get_executor(),
+                            bind_handler(move(*this), code, 0)
+                        );
+                    }
+                    _step = 2;
+
+                do_read:
+                    return _stream.async_read_some(
+                        _buffer.prepare(read_size(_buffer, 1536)),
+                        move(*this)
+                    );
+
+                case 1:
+                    break;
+
+                case 2:
+                    _step = 3;
+                    [[fallthrough]];
+
+                case 3:
+                    if (code) {
+                        _result = false;
+                        break;
+                    }
+
+                    _buffer.commit(bytes_transferred);
+                    _result = is_ssl_handshake(_buffer.data());
+
+                    if (!indeterminate(_result)) break;
+
+                    goto do_read;
+
+                default:
+                    break;
+            }
+
+            _handler(code, _result);
+        }
+    };
 
     class Detector : public enable_shared_from_this<Detector> {
     private:
@@ -258,7 +165,20 @@ namespace server {
 
         void run();
 
+    private:
         void on_detect(error_code code, tribool secured);
+
+        template<class Stream, class Buffer, class Token>
+        RESULT_TYPE(Token, HANDLER_SIG)
+        async_detect_ssl(Stream &stream, Buffer &buffer, Token &&token) {
+            async_completion<Token, void(error_code, tribool)> init{token};
+
+            DetectSslOp<Stream, Buffer, HANDLER_TYPE(Token, HANDLER_SIG)>{
+                stream, buffer, init.completion_handler
+            }(error_code{}, 0);
+
+            return init.result.get();
+        }
     };
 }
 
