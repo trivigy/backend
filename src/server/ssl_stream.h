972d592fe234c0dbdd2fcd8adc9384a6b0095eac
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-05-05 17:10:56 -0400

    Refactor SslStream class to conform it to the rest of the namespace

--- src/server/ssl_stream.h
+++ src/server/ssl_stream.h
@@ -9,271 +9,251 @@
 #include <type_traits>
 #include <utility>
 
-template<class NextLayer>
-class ssl_stream : public boost::asio::ssl::stream_base {
-    static_assert(std::is_same<NextLayer, boost::asio::ip::tcp::socket>::value, "NextLayer requirements not met");
-    using stream_type = boost::asio::ssl::stream<NextLayer>;
+using namespace std;
 
-private:
-    std::unique_ptr<stream_type> p_;
-    boost::asio::ssl::context* ctx_;
+namespace server {
+    namespace ssl = boost::asio::ssl;
+    using boost::system::error_code;
+    using boost::asio::ip::tcp;
+    using boost::asio::ssl::verify_mode;
 
-public:
-    using native_handle_type = typename stream_type::native_handle_type;
-    using impl_struct = typename stream_type::impl_struct;
-    using next_layer_type = typename stream_type::next_layer_type;
-    using lowest_layer_type = typename stream_type::lowest_layer_type;
-    using executor_type = typename stream_type::executor_type;
+    template<class NextLayer>
+    class SslStream : public ssl::stream_base {
+        using stream_type = ssl::stream<NextLayer>;
 
-    ssl_stream(
-        boost::asio::ip::tcp::socket socket,
-        boost::asio::ssl::context& ctx
-    ) : p_(new stream_type{socket.get_executor().context(), ctx}),
-        ctx_(&ctx) {
-        p_->next_layer() = std::move(socket);
-    }
+    private:
+        unique_ptr<stream_type> _p;
+        ssl::context* _ctx;
 
-    ssl_stream(ssl_stream&& other) :
-        p_(new stream_type(other.get_executor().context(), *other.ctx_)),
-        ctx_(other.ctx_) {
-        using std::swap;
-        swap(p_, other.p_);
-    }
+    public:
+        using native_handle_type = typename stream_type::native_handle_type;
+        using impl_struct = typename stream_type::impl_struct;
+        using next_layer_type = typename stream_type::next_layer_type;
+        using lowest_layer_type = typename stream_type::lowest_layer_type;
+        using executor_type = typename stream_type::executor_type;
 
-    ssl_stream& operator=(ssl_stream&& other) {
-        std::unique_ptr<stream_type> p(new stream_type{other.get_executor().context(), other.ctx_});
-        using std::swap;
-        swap(p_, p);
-        swap(p_, other.p_);
-        ctx_ = other.ctx_;
-        return *this;
-    }
+        SslStream(tcp::socket socket, ssl::context& ctx) :
+            _p(new stream_type{socket.get_executor().context(), ctx}),
+            _ctx(&ctx) {
+            _p->next_layer() = move(socket);
+        }
 
-    executor_type get_executor() noexcept {
-        return p_->get_executor();
-    }
+        SslStream(SslStream&& other) noexcept :
+            _p(new stream_type(other.get_executor().context(), *other._ctx)),
+            _ctx(other._ctx) {
+            using std::swap;
+            swap(_p, other._p);
+        }
 
-    native_handle_type native_handle() {
-        return p_->native_handle();
-    }
+        SslStream& operator=(SslStream&& other) noexcept {
+            std::unique_ptr<stream_type> p(new stream_type(
+                other.get_executor().context(), 
+                *other._ctx
+            ));
+            using std::swap;
+            swap(_p, p);
+            swap(_p, other._p);
+            _ctx = other._ctx;
+            return *this;
+        }
 
-    next_layer_type const&next_layer() const {
-        return p_->next_layer();
-    }
+        executor_type get_executor() noexcept {
+            return _p->get_executor();
+        }
 
-    next_layer_type&next_layer() {
-        return p_->next_layer();
-    }
+        native_handle_type native_handle() {
+            return _p->native_handle();
+        }
 
-    lowest_layer_type&lowest_layer() {
-        return p_->lowest_layer();
-    }
+        next_layer_type const&next_layer() const {
+            return _p->next_layer();
+        }
 
-    lowest_layer_type const&lowest_layer() const {
-        return p_->lowest_layer();
-    }
+        next_layer_type&next_layer() {
+            return _p->next_layer();
+        }
 
-    void set_verify_mode(boost::asio::ssl::verify_mode v) {
-        p_->set_verify_mode(v);
-    }
+        lowest_layer_type&lowest_layer() {
+            return _p->lowest_layer();
+        }
 
-    boost::system::error_code set_verify_mode(
-        boost::asio::ssl::verify_mode v,
-        boost::system::error_code& ec
-    ) {
-        return p_->set_verify_mode(v, ec);
-    }
+        lowest_layer_type const&lowest_layer() const {
+            return _p->lowest_layer();
+        }
 
-    void set_verify_depth(int depth) {
-        p_->set_verify_depth(depth);
-    }
+        void set_verify_mode(boost::asio::ssl::verify_mode v) {
+            _p->set_verify_mode(v);
+        }
 
-    boost::system::error_code set_verify_depth(
-        int depth,
-        boost::system::error_code& ec
-    ) {
-        return p_->set_verify_depth(depth, ec);
-    }
+        error_code set_verify_mode(verify_mode v, error_code& code) {
+            return _p->set_verify_mode(v, code);
+        }
 
-    template<class VerifyCallback> void set_verify_callback(
-        VerifyCallback callback
-    ) {
-        p_->set_verify_callback(callback);
-    }
+        void set_verify_depth(int depth) {
+            _p->set_verify_depth(depth);
+        }
 
-    template<class VerifyCallback>
-    boost::system::error_code set_verify_callback(
-        VerifyCallback callback,
-        boost::system::error_code& ec
-    ) {
-        return p_->set_verify_callback(callback, ec);
-    }
+        error_code set_verify_depth(int depth, error_code& code) {
+            return _p->set_verify_depth(depth, code);
+        }
 
-    void handshake(handshake_type type) {
-        p_->handshake(type);
-    }
+        template<class VerifyCallback> 
+        void set_verify_callback(VerifyCallback callback) {
+            _p->set_verify_callback(callback);
+        }
 
-    boost::system::error_code handshake(
-        handshake_type type,
-        boost::system::error_code& ec
-    ) {
-        return p_->handshake(type, ec);
-    }
+        template<class VerifyCallback>
+        error_code set_verify_callback(
+            VerifyCallback callback,
+            error_code& code
+        ) {
+            return _p->set_verify_callback(callback, code);
+        }
 
-    template<class ConstBufferSequence>
-    void handshake(
-        handshake_type type,
-        ConstBufferSequence const& buffers
-    ) {
-        p_->handshake(type, buffers);
-    }
+        void handshake(handshake_type type) {
+            _p->handshake(type);
+        }
 
-    template<class ConstBufferSequence>
-    boost::system::error_code handshake(
-        handshake_type type,
-        ConstBufferSequence const& buffers,
-        boost::system::error_code& ec
-    ) {
-        return p_->handshake(type, buffers, ec);
-    }
+        error_code handshake(handshake_type type, error_code& code) {
+            return _p->handshake(type, code);
+        }
 
-    template<class HandshakeHandler>
-    BOOST_ASIO_INITFN_RESULT_TYPE(
-        HandshakeHandler,
-        void(boost::system::error_code)
-    )
-    async_handshake(handshake_type type,
-        BOOST_ASIO_MOVE_ARG(HandshakeHandler) handler
-    ) {
-        return p_->async_handshake(
-            type,
-            BOOST_ASIO_MOVE_CAST(HandshakeHandler)(handler)
+        template<class ConstBufferSeq>
+        void handshake(handshake_type type, ConstBufferSeq const& buffers) {
+            _p->handshake(type, buffers);
+        }
+
+        template<class ConstBufferSeq>
+        boost::system::error_code handshake(
+            handshake_type type,
+            ConstBufferSeq const& buffers,
+            error_code& code
+        ) {
+            return _p->handshake(type, buffers, code);
+        }
+
+        template<class HandshakeHandler>
+        BOOST_ASIO_INITFN_RESULT_TYPE(
+            HandshakeHandler,
+            void(boost::system::error_code)
+        )
+        async_handshake(handshake_type type,
+            BOOST_ASIO_MOVE_ARG(HandshakeHandler) handler
+        ) {
+            return _p->async_handshake(type, handler);
+        }
+
+        template<class ConstBufferSeq, class BufferedHandshakeHandler>
+        BOOST_ASIO_INITFN_RESULT_TYPE(
+            BufferedHandshakeHandler,
+            void (boost::system::error_code, std::size_t)
+        )
+        async_handshake(
+            handshake_type type,
+            ConstBufferSeq const& buffers,
+            BOOST_ASIO_MOVE_ARG(BufferedHandshakeHandler) handler
+        ) {
+            return _p->async_handshake(type, buffers, handler);
+        }
+
+        void shutdown() {
+            _p->shutdown();
+        }
+
+        error_code shutdown(error_code& code) {
+            return _p->shutdown(code);
+        }
+
+        template<class ShutdownHandler>
+        BOOST_ASIO_INITFN_RESULT_TYPE(
+            ShutdownHandler,
+            void (boost::system::error_code)
+        )
+        async_shutdown(BOOST_ASIO_MOVE_ARG(ShutdownHandler) handler) {
+            return _p->async_shutdown(handler);
+        }
+
+        template<class ConstBufferSeq>
+        size_t write_some(ConstBufferSeq const& buffers) {
+            return _p->write_some(buffers);
+        }
+
+        template<class ConstBufferSeq>
+        size_t write_some(ConstBufferSeq const& buffers, error_code& code) {
+            return _p->write_some(buffers, code);
+        }
+
+        template<class ConstBufferSeq, class WriteHandler>
+        BOOST_ASIO_INITFN_RESULT_TYPE(
+            WriteHandler,
+            void (boost::system::error_code, std::size_t)
+        )
+        async_write_some(
+            ConstBufferSeq const& buffers,
+            BOOST_ASIO_MOVE_ARG(WriteHandler) handler
+        ) {
+            return _p->async_write_some(buffers, handler);
+        }
+
+        template<class MutableBufferSequence>
+        size_t read_some(MutableBufferSequence const& buffers) {
+            return _p->read_some(buffers);
+        }
+
+        template<class MutableBufferSequence>
+        size_t read_some(
+            MutableBufferSequence const& buffers,
+            error_code& code
+        ) {
+            return _p->read_some(buffers, code);
+        }
+
+        template<class MutableBufferSequence, class ReadHandler>
+        BOOST_ASIO_INITFN_RESULT_TYPE(
+            ReadHandler,
+            void(boost::system::error_code, std::size_t)
+        )
+        async_read_some(
+            MutableBufferSequence const& buffers,
+            BOOST_ASIO_MOVE_ARG(ReadHandler) handler
+        ) {
+            return _p->async_read_some(buffers, handler);
+        }
+
+        template<class SyncStream>
+        friend void teardown(
+            boost::beast::websocket::role_type,
+            SslStream<SyncStream>& stream,
+            error_code& code
         );
-    }
 
-    template<class ConstBufferSequence, class BufferedHandshakeHandler>
-    BOOST_ASIO_INITFN_RESULT_TYPE(
-        BufferedHandshakeHandler,
-        void (boost::system::error_code, std::size_t)
-    )
-    async_handshake(
-        handshake_type type,
-        ConstBufferSequence const& buffers,
-        BOOST_ASIO_MOVE_ARG(BufferedHandshakeHandler) handler
-    ) {
-        return p_->async_handshake(
-            type,
-            buffers,
-            BOOST_ASIO_MOVE_CAST(BufferedHandshakeHandler)(handler)
+        template<class AsyncStream, class TeardownHandler>
+        friend void async_teardown(
+            boost::beast::websocket::role_type,
+            SslStream<AsyncStream>& stream,
+            TeardownHandler&& handler
         );
-    }
-
-    void shutdown() {
-        p_->shutdown();
-    }
-
-    boost::system::error_code shutdown(boost::system::error_code& ec) {
-        return p_->shutdown(ec);
-    }
-
-    template<class ShutdownHandler>
-    BOOST_ASIO_INITFN_RESULT_TYPE(
-        ShutdownHandler,
-        void (boost::system::error_code)
-    )
-    async_shutdown(BOOST_ASIO_MOVE_ARG(ShutdownHandler) handler) {
-        return p_->async_shutdown(
-            BOOST_ASIO_MOVE_CAST(ShutdownHandler)(handler)
-        );
-    }
-
-    template<class ConstBufferSequence>
-    std::size_t write_some(ConstBufferSequence const& buffers) {
-        return p_->write_some(buffers);
-    }
-
-    template<class ConstBufferSequence>
-    std::size_t write_some(
-        ConstBufferSequence const& buffers,
-        boost::system::error_code& ec
-    ) {
-        return p_->write_some(buffers, ec);
-    }
-
-    template<class ConstBufferSequence, class WriteHandler>
-    BOOST_ASIO_INITFN_RESULT_TYPE(
-        WriteHandler,
-        void (boost::system::error_code, std::size_t)
-    )
-    async_write_some(
-        ConstBufferSequence const& buffers,
-        BOOST_ASIO_MOVE_ARG(WriteHandler) handler
-    ) {
-        return p_->async_write_some(buffers,
-            BOOST_ASIO_MOVE_CAST(WriteHandler)(handler)
-        );
-    }
-
-    template<class MutableBufferSequence>
-    std::size_t read_some(MutableBufferSequence const& buffers) {
-        return p_->read_some(buffers);
-    }
-
-    template<class MutableBufferSequence>
-    std::size_t read_some(
-        MutableBufferSequence const& buffers,
-        boost::system::error_code& ec
-    ) {
-        return p_->read_some(buffers, ec);
-    }
-
-    template<class MutableBufferSequence, class ReadHandler>
-    BOOST_ASIO_INITFN_RESULT_TYPE(
-        ReadHandler,
-        void(boost::system::error_code, std::size_t)
-    )
-    async_read_some(
-        MutableBufferSequence const& buffers,
-        BOOST_ASIO_MOVE_ARG(ReadHandler) handler
-    ) {
-        return p_->async_read_some(buffers,
-            BOOST_ASIO_MOVE_CAST(ReadHandler)(handler)
-        );
-    }
+    };
 
     template<class SyncStream>
-    friend void teardown(
-        boost::beast::websocket::role_type,
-        ssl_stream<SyncStream>& stream,
-        boost::system::error_code& ec
-    );
+    inline void teardown(
+        boost::beast::websocket::role_type role,
+        SslStream<SyncStream>& stream,
+        boost::system::error_code& code
+    ) {
+        using boost::beast::websocket::teardown;
+        teardown(role, *stream._p, code);
+    }
 
     template<class AsyncStream, class TeardownHandler>
-    friend void async_teardown(
-        boost::beast::websocket::role_type,
-        ssl_stream<AsyncStream>& stream,
+    inline void async_teardown(
+        boost::beast::websocket::role_type role,
+        SslStream<AsyncStream>& stream,
         TeardownHandler&& handler
-    );
-};
-
-template<class SyncStream>
-inline void teardown(
-    boost::beast::websocket::role_type role,
-    ssl_stream<SyncStream>& stream,
-    boost::system::error_code& ec
-) {
-    using boost::beast::websocket::teardown;
-    teardown(role, *stream.p_, ec);
-}
-
-template<class AsyncStream, class TeardownHandler>
-inline void async_teardown(
-    boost::beast::websocket::role_type role,
-    ssl_stream<AsyncStream>& stream,
-    TeardownHandler&& handler
-) {
-    using boost::beast::websocket::async_teardown;
-    async_teardown(role, *stream.p_, std::forward<TeardownHandler>(handler));
+    ) {
+        using boost::beast::websocket::async_teardown;
+        async_teardown(role, *stream._p, forward<TeardownHandler>(handler));
+    }
 }
 
 #endif
